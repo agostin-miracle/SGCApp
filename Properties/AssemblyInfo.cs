0442947613f37f2cbd78b1fa9f9401882ef2c422
Agostinho da Silva Milagres <agostinho.milagres@petcare.com.br>
2024-03-31 12:14:32 -0300

    Rev: 1.0.1.11

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -3,7 +3,11 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("SCGApp")]
 [assembly: AssemblyDescription("Source Code Generator Desktop")]
-[assembly: AssemblyConfiguration("")]
+#if DEBUG
+[assembly: AssemblyConfiguration("Versão do Depurador")]
+#else
+[assembly: AssemblyConfiguration("Versão de Produção")]
+#endif
 [assembly: AssemblyCompany("AGOSTIN SERVICOS DE INFORMATICA E GERENCIAMENTO LTDA.")]
 [assembly: AssemblyProduct("SCG")]
 [assembly: AssemblyCopyright("Copyright ©  2012")]
@@ -11,5 +15,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("c669c072-aa97-42ff-ad55-67856f3bbe52")]
-[assembly: AssemblyVersion("1.1.0.10")]
-[assembly: AssemblyFileVersion("1.1.0.10")]
+[assembly: AssemblyVersion("1.1.0.11")]
+[assembly: AssemblyFileVersion("1.1.0.11")]
